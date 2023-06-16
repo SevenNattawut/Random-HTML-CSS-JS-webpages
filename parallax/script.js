cf85fecfa6b5fc9ecd0caf777e3aa58f03875d01
SevenNattawut <90675334+SevenNattawut@users.noreply.github.com>
2023-06-16 17:00:23 +0700

    Add files via upload

--- parallax/script.js
+++ parallax/script.js
@@ -3,6 +3,9 @@
     const input = document.getElementById("title-text");
     const msg = document.getElementById("msg");
     const displayText = document.querySelector(".display-text");
+    const head = document.querySelector(".headArea");
+    const hpText = document.querySelector(".hp-text");
+    const bottomDiv = document.querySelector(".bottom");
 
     function parallaxFx() {
         let currentY = window.scrollY;
@@ -14,20 +17,34 @@
         if (currentY >= 200 && currentY < 1000) {
             displayText.style.transform = `translate(-50%, ${(currentY-200) * -0.25}%)`;
         }
-        if (currentY >= 1010) {
-            var opacity = 1 - ((currentY - 1010) / 400);
+        if (currentY >= 1150) {
+            var opacity = 1 - ((currentY - 1150) / 400);
             displayText.style.opacity = opacity;
-          }
+        }
+        if (currentY >= 5000) {
+            var opacity = ((currentY - 5400) / 150);
+            bottomDiv.style.opacity = opacity;
+        }
     }
 
     function updateText() {
         msg.innerText = `${input.value}`;
     }
 
+    function updateTextHp(event) {
+        if (event.type === "mouseover") {
+            hpText.innerText = "Have a good day :D";
+        } else if (event.type === "mouseout") {
+            hpText.innerText = "hover on her head to headpat";
+        }
+    }
+
 
     function run() {
         document.addEventListener("scroll", parallaxFx);
         input.addEventListener("input", updateText);
+        head.addEventListener("mouseover", updateTextHp);
+        head.addEventListener("mouseout", updateTextHp);
     }
 
     run();
